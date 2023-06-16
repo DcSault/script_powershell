f100b0585ee2634fbdbdda24f98be855fab4513f
NotSault <rosiquevictor@gmail.com>
2023-06-16 14:42:59 +0200

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,4 +1,30 @@
-Write-Output "http://monserveur.com/script1.ps1"
-Write-Output "http://monserveur.com/script2.ps1"
-Write-Output "http://monserveur.com/script3.ps1"
+# Définition d'un dictionnaire de scripts
+$scripts = @{}
 
+# Ajout des scripts au dictionnaire
+$scripts["1"] = @{
+    "Name" = "Desactiver redirection IE"
+    "URL" = "https://raw.githubusercontent.com/DcSault/script_powershell/main/desactiver_redirection_ie.ps1"
+}
+
+# Affichage des scripts disponibles
+Write-Host "Scripts disponibles :"
+$scripts.Keys | ForEach-Object {
+    Write-Host ("{0} - {1}" -f $_, $scripts[$_].Name)
+}
+
+# Demande à l'utilisateur de choisir un script à exécuter
+$scriptChoice = Read-Host -Prompt "Entrez le numéro du script à exécuter"
+
+# Vérifie si le script choisi existe dans le dictionnaire
+if ($scripts.ContainsKey($scriptChoice)) {
+    # Téléchargement du script choisi
+    $chosenScript = $scripts[$scriptChoice]
+    $scriptOutput = Join-Path $env:TEMP ($chosenScript.Name + ".ps1")
+    Invoke-WebRequest -Uri $chosenScript.URL -OutFile $scriptOutput -UseBasicParsing
+
+    # Exécution du script choisi
+    . $scriptOutput
+} else {
+    Write-Host "Le script choisi n'existe pas."
+}
