e1a336d97e2b31166e1dc93d27b2e268e772795b
NotSault <rosiquevictor@gmail.com>
2023-06-16 16:06:25 +0200

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,90 +1,69 @@
-# Définition d'une hiérarchie de scripts
-$scripts = @{}
+# Initialiser le tableau de scripts
+$scriptFolders = @()
 
-# Ajout des scripts au dictionnaire
-$scripts["1"] = @{
-    "Name" = "Dossier 1"
-    "Scripts" = @{
-        "1" = @{
-            "Name" = "Desactiver redirection IE"
-            "URL" = "https://raw.githubusercontent.com/DcSault/script_powershell/main/desactiver_redirection_ie.ps1"
+# Définir les dossiers de scripts
+$folder1 = @{
+    Name = "Trousse_A_Outils";
+    Scripts = @(
+        @{
+            Name = "IE_BHO";
+            Url = "https://raw.githubusercontent.com/DcSault/script_powershell/main/desactiver_redirection_ie.ps1"
+        },
+        @{
+            Name = "Exemple 3";
+            Url = "https://raw.githubusercontent.com/DcSault/script_powershell/main/script2.ps1"
         }
-        "2" = @{
-            "Name" = "Nouveau script"
-            "URL" = "https://example.com/nouveau_script.ps1"
-        }
-    }
+    )
 }
 
-$scripts["2"] = @{
-    "Name" = "Dossier 2"
-    "Scripts" = @{
-        "1" = @{
-            "Name" = "Script 1 dans Dossier 2"
-            "URL" = "https://example.com/script1_dossier2.ps1"
+$folder2 = @{
+    Name = "Network_Tool";
+    Scripts = @(
+        @{
+            Name = "Ping Tools";
+            Url = "https://raw.githubusercontent.com/DcSault/script_powershell/main/Script/Ping_V2.5%20-%20Beta%20-%20Json.ps1"
+        },
+        @{
+            Name = "Exemple 2";
+            Url = "https://raw.githubusercontent.com/DcSault/script_powershell/main/script4.ps1"
         }
-        "2" = @{
-            "Name" = "Script 2 dans Dossier 2"
-            "URL" = "https://example.com/script2_dossier2.ps1"
-        }
-    }
+    )
 }
 
-# Boucle principale
-while ($true) {
-    # Affichage des scripts disponibles
-    Write-Host "`n=== MENU PRINCIPAL ==="
-    $scripts.Keys | ForEach-Object {
-        Write-Host ("{0} - {1}" -f $_, $scripts[$_].Name)
+# Ajouter les dossiers de scripts à la liste
+$scriptFolders += $folder1
+$scriptFolders += $folder2
+
+do {
+    Write-Host "`n=== DOSSIERS DISPONIBLES ==="
+    for ($i=0; $i -lt $scriptFolders.Count; $i++) {
+        Write-Host "($($i+1)) $($scriptFolders[$i].Name)"
     }
-    Write-Host "Q - Quitter"
+    Write-Host "(Q) Quitter"
 
-    # Demande à l'utilisateur de choisir un script à exécuter
-    $folderChoice = Read-Host -Prompt "`nEntrez le numéro du dossier à ouvrir ou 'Q' pour quitter"
+    $folderChoice = Read-Host "`nEntrez le numéro du dossier à ouvrir ou 'Q' pour quitter"
+    if ($folderChoice -eq 'Q') { exit }
 
-    # Vérifie si l'utilisateur a choisi de quitter
-    if ($folderChoice -eq "Q") {
-        break
-    }
+    $chosenFolder = $scriptFolders[$folderChoice - 1]
 
-    # Vérifie si le dossier choisi existe dans le dictionnaire
-    if ($scripts.ContainsKey($folderChoice)) {
-        $chosenFolder = $scripts[$folderChoice]
-        while ($true) {
-            # Affichage des scripts disponibles
-            Write-Host "`n=== MENU DU DOSSIER {0} ===" -f $chosenFolder.Name
-            $chosenFolder.Scripts.Keys | ForEach-Object {
-                Write-Host ("{0} - {1}" -f $_, $chosenFolder.Scripts[$_].Name)
-            }
-            Write-Host "R - Retour"
-            Write-Host "Q - Quitter"
-
-            # Demande à l'utilisateur de choisir un script à exécuter
-            $scriptChoice = Read-Host -Prompt "`nEntrez le numéro du script à exécuter, 'R' pour revenir ou 'Q' pour quitter"
-
-            # Vérifie si l'utilisateur a choisi de quitter ou de revenir
-            if ($scriptChoice -eq "Q") {
-                exit
-            } elseif ($scriptChoice -eq "R") {
-                break
-            }
-
-            # Vérifie si le script choisi existe dans le dictionnaire
-            if ($chosenFolder.Scripts.ContainsKey($scriptChoice)) {
-                # Téléchargement du script choisi
-                Write-Host "`n=== TÉLÉCHARGEMENT DU SCRIPT ==="
-                $chosenScript = $chosenFolder.Scripts[$scriptChoice]
-                $scriptOutput = Join-Path $env:TEMP ($chosenScript.Name + ".ps1")
-                Invoke-WebRequest -Uri $chosenScript.URL -OutFile $scriptOutput -UseBasicParsing
-
-                # Exécution du script choisi
-                Write-Host "`n=== EXÉCUTION DU SCRIPT ==="
-                . $scriptOutput
-            } else {
-                Write-Host "`nErreur : Le script choisi n'existe pas."
-            }
+    do {
+        Write-Host "`n=== SCRIPTS DISPONIBLES DANS $($chosenFolder.Name) ==="
+        for ($i=0; $i -lt $chosenFolder.Scripts.Count; $i++) {
+            Write-Host "($($i+1)) $($chosenFolder.Scripts[$i].Name)"
         }
-    } else {
-        Write-Host "`nErreur : Le dossier choisi n'existe pas."
-    }
-}
+        Write-Host "(Q) Retour"
+
+        $scriptChoice = Read-Host "`nEntrez le numéro du script à exécuter ou 'Q' pour revenir"
+        if ($scriptChoice -eq 'Q') { break }
+
+        $chosenScript = $chosenFolder.Scripts[$scriptChoice - 1]
+
+        # Télécharger et exécuter le script
+        $output = "$env:TEMP\$($chosenScript.Name).ps1"
+        Invoke-WebRequest -Uri $chosenScript.Url -OutFile $output
+        & $output
+        Remove-Item $output
+
+    } until ($false)
+
+} until ($false)
