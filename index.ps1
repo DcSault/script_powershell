de132ba149e00189b7fed6b0dc64e1d897377282
NotSault <rosiquevictor@gmail.com>
2023-06-16 14:49:42 +0200

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -7,24 +7,40 @@ $scripts["1"] = @{
     "URL" = "https://raw.githubusercontent.com/DcSault/script_powershell/main/desactiver_redirection_ie.ps1"
 }
 
-# Affichage des scripts disponibles
-Write-Host "Scripts disponibles :"
-$scripts.Keys | ForEach-Object {
-    Write-Host ("{0} - {1}" -f $_, $scripts[$_].Name)
+$scripts["2"] = @{
+    "Name" = "Nouveau script"
+    "URL" = "https://example.com/nouveau_script.ps1"
 }
 
-# Demande à l'utilisateur de choisir un script à exécuter
-$scriptChoice = Read-Host -Prompt "Entrez le numéro du script à exécuter"
+# Boucle principale
+while ($true) {
+    # Affichage des scripts disponibles
+    Write-Host "`n=== MENU PRINCIPAL ==="
+    $scripts.Keys | ForEach-Object {
+        Write-Host ("{0} - {1}" -f $_, $scripts[$_].Name)
+    }
+    Write-Host "Q - Quitter"
 
-# Vérifie si le script choisi existe dans le dictionnaire
-if ($scripts.ContainsKey($scriptChoice)) {
-    # Téléchargement du script choisi
-    $chosenScript = $scripts[$scriptChoice]
-    $scriptOutput = Join-Path $env:TEMP ($chosenScript.Name + ".ps1")
-    Invoke-WebRequest -Uri $chosenScript.URL -OutFile $scriptOutput -UseBasicParsing
+    # Demande à l'utilisateur de choisir un script à exécuter
+    $scriptChoice = Read-Host -Prompt "`nEntrez le numéro du script à exécuter ou 'Q' pour quitter"
 
-    # Exécution du script choisi
-    . $scriptOutput
-} else {
-    Write-Host "Le script choisi n'existe pas."
+    # Vérifie si l'utilisateur a choisi de quitter
+    if ($scriptChoice -eq "Q") {
+        break
+    }
+
+    # Vérifie si le script choisi existe dans le dictionnaire
+    if ($scripts.ContainsKey($scriptChoice)) {
+        # Téléchargement du script choisi
+        Write-Host "`n=== TÉLÉCHARGEMENT DU SCRIPT ==="
+        $chosenScript = $scripts[$scriptChoice]
+        $scriptOutput = Join-Path $env:TEMP ($chosenScript.Name + ".ps1")
+        Invoke-WebRequest -Uri $chosenScript.URL -OutFile $scriptOutput -UseBasicParsing
+
+        # Exécution du script choisi
+        Write-Host "`n=== EXÉCUTION DU SCRIPT ==="
+        . $scriptOutput
+    } else {
+        Write-Host "`nErreur : Le script choisi n'existe pas."
+    }
 }
